3baf34c55bed0fe0760b46372711849b0064e881
gino <gamexgaming1997@gmail.com>
2022-07-02 22:52:09 +0800

    finished

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { FaEthereum, FaClock } from 'react-icons/fa';
 import './scss/Styles.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
