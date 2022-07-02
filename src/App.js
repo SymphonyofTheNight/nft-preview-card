19a94f875e0aa53c8641a7cbf8f3cbfa92f6a076
gino <gamexgaming1997@gmail.com>
2022-07-02 22:50:54 +0800

    nft-finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import { FaEthereum, FaClock } from 'react-icons/fa';
 import './scss/Styles.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
 
-    const [api, setApi] = useState();
-
-    useEffect(()=> {
-        axios.get('https://api.coingecko.com/api/v3/coins/ethereum')
-        .then(val => setApi(val)).catch(err => console.log(err));
-    },[])
-
-    console.log(api)
+    const [value, setValue] = useState('0.041');
 
     const card = <div className='Card'>
         <section>
           <div className='overlay'>
-
+              <div className='img-container'/>
+          </div>
+          <div className='nft-name-container'>
+              <button className='text'>
+                Crystal Diamond #9875
+              </button>
+          </div>
+          <div className='nft-preview-card-description'>
+            <span className='text'>
+                Our Crystal Diamond collection promotes balance and calm.
+            </span>
+          </div>
+          <div className='price-nft'>
+              <FaEthereum className='eth'/>
+              <span className='eth-price-text'>
+                {value} ETH
+              </span>
+              <span className='time-nft-text'>
+                <FaClock className='clock'/> &nbsp; 3 days left
+              </span>
+          </div>
+          <div className='nft-creator-container'>
+              <div className='inner-creator-container'>
+                <div className='creator-profile-img'/>
+                <span className='text'>
+                  &nbsp; Creation of <span className='name'>Fannix</span>
+                </span>
+              </div>
           </div>
         </section>
     </div>
