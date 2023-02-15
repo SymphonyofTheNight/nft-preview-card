37bdd9db8d0617f2a43a531dc759f4618f468288
gino <gamexgaming1997@gmail.com>
2023-02-15 14:17:13 +0800

    updates

--- src/App.js
+++ src/App.js
@@ -5,55 +5,55 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
 
-    const [value, setValue] = useState('0.041');
+  const [value] = useState('0.041');
 
-    const card = <div className='Card'>
-        <section>
-          <div className='overlay'>
-              <div className='img-container'/>
-          </div>
-          <div className='nft-name-container'>
-              <button className='text'>
-                Crystal Diamond #9875
-              </button>
-          </div>
-          <div className='nft-preview-card-description'>
-            <span className='text'>
-                Our Crystal Diamond collection promotes balance and calm.
-            </span>
-          </div>
-          <div className='price-nft'>
-              <FaEthereum className='eth'/>
-              <span className='eth-price-text'>
-                {value} ETH
-              </span>
-              <span className='time-nft-text'>
-                <FaClock className='clock'/> &nbsp; 3 days left
-              </span>
-          </div>
-          <div className='nft-creator-container'>
-              <div className='inner-creator-container'>
-                <div className='creator-profile-img'/>
-                <span className='text'>
-                  &nbsp; Creation of <span className='name'>Fannix</span>
-                </span>
-              </div>
-          </div>
-        </section>
-    </div>
-
-    const creator = <div className='TextContainer'>
+  const card = <div className='Card'>
+    <section>
+      <div className='overlay'>
+        <div className='img-container' />
+      </div>
+      <div className='nft-name-container'>
+        <button className='text'>
+          Crystal Diamond #9875
+        </button>
+      </div>
+      <div className='nft-preview-card-description'>
         <span className='text'>
-            Coded by <span className='Name'>
-              Gino Dela Vega
-            </span>
+          Our Crystal Diamond collection promotes balance and calm.
         </span>
-    </div>
+      </div>
+      <div className='price-nft'>
+        <FaEthereum className='eth' />
+        <span className='eth-price-text'>
+          {value} ETH
+        </span>
+        <span className='time-nft-text'>
+          <FaClock className='clock' /> &nbsp; 3 days left
+        </span>
+      </div>
+      <div className='nft-creator-container'>
+        <div className='inner-creator-container'>
+          <div className='creator-profile-img' />
+          <span className='text'>
+            &nbsp; Creation of <span className='name'>Fannix</span>
+          </span>
+        </div>
+      </div>
+    </section>
+  </div>
+
+  const creator = <div className='TextContainer'>
+    <span className='text'>
+      Coded by <span className='Name'>
+        Gino Dela Vega
+      </span>
+    </span>
+  </div>
 
   return (
     <div className='App col-lg-12 m-0 p-0'>
-        {card}
-        {creator}
+      {card}
+      {creator}
     </div>
   )
 }
